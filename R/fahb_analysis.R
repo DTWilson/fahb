81f0ab3ea00530a5ca0c5e81c8a50428a453887e
Duncan Wilson <meddwilb@leeds.ac.uk>
2026-04-16 09:10:10 +0100

    Main analysis functions

--- R/fahb_analysis.R
+++ R/fahb_analysis.R
@@ -1,11 +1,16 @@
-new_fahb_analysis <- function(){
-  structure(list(),
+new_fahb_analysis <- function(PC_stats, Bayes_stats,
+                              PP_rec_times, so_post_hps,
+                              model_fit){
+  structure(list(PC_stats = PC_stats, Bayes_stats = Bayes_stats,
+                 PP_rec_times = PP_rec_times, so_post_hps = so_post_hps,
+                 model_fit = model_fit),
             class = "fahb_analysis")
 }
 
 fahb_analysis <- function(n_pilot, t_pilot,
-                          bayes_model,
-                          problem){
+                          problem, 
+                          bayes_model = NULL){
+  m <- problem$m
   
   # Get standard PC summaries
   n_p <- sum(n_pilot)
@@ -18,8 +23,6 @@ fahb_analysis <- function(n_pilot, t_pilot,
     bayes_model <- compile_bayes_model(n_pilot, t_pilot, problem)
   }
   
-  if(m_p == 1 && int_data$t == 0) return(-1)
-  
   # Generate posterior samples
   output <- capture.output(fit <- suppressWarnings(update(bayes_model, 
                                                           backend = "cmdstanr",
@@ -31,9 +34,9 @@ fahb_analysis <- function(n_pilot, t_pilot,
   
   # Get vectors of posterior samples for the mean and SD parameters of the 
   # log-normal model for site recruitment rates
-  s <- as_draws(fit)
-  beta_0 <- extract_variable(s, "b_Intercept")
-  sd_r <- extract_variable(s, "sd_c__Intercept")
+  s <- brms::as_draws(fit)
+  beta_0 <- posterior::extract_variable(s, "b_Intercept")
+  sd_r <- posterior::extract_variable(s, "sd_c__Intercept")
   
   # First deal with cases where not all sites have yet opened
   if(m - m_p > 0){
@@ -42,26 +45,22 @@ fahb_analysis <- function(n_pilot, t_pilot,
     # Using the posterior samples of the log-normal recruitment rate model.
     # Each column of u is a yearly rate at that site, each row a sample from the 
     # posterior.
-    us <- sapply(1:(m-m_p), function(x) exp(rnorm(length(beta_0), beta_0, sd_r)))
+    us <- sapply(1:(m-m_p), function(x) exp(stats::rnorm(length(beta_0), beta_0, sd_r)))
     
     # Add to that matrix the posterior samples for the sites which have already
     # opened
-    r <- ranef(fit, summary = F)
+    r <- brms::ranef(fit, summary = F)
     us <- cbind(us, exp(r$c[,1:m_p,1] + beta_0))
     
     # For site setup rates we have a simple conjugate analysis
-    if(!internal){
-      
-    }
     setup_r_a1 <- setup_r_a + m_p
-    setup_r_b1 <- setup_r_b + site_t
+    setup_r_b1 <- setup_r_b + problem$p_t
     
     # We can then generate samples from the posterior of setup rate
-    setup_rates <- rgamma(nrow(us), setup_r_a1, setup_r_b1)
+    setup_rates <- stats::rgamma(nrow(us), setup_r_a1, setup_r_b1)
     
-    if(internal){
       # Now sample actual site setup times from the posterior predictive
-      setup_times <- t(sapply(setup_rates, function(x) cumsum(rexp(m-m_p, x))))
+      setup_times <- t(sapply(setup_rates, function(x) cumsum(stats::rexp(m-m_p, x))))
       if(m-m_p == 1) setup_times <- t(setup_times)
       
       # Add pilot sites using a setup time of 0
@@ -70,47 +69,27 @@ fahb_analysis <- function(n_pilot, t_pilot,
       site_matrix <- cbind(us, setup_times)
       
       rec_times <- apply(site_matrix, 1, post_pred_rec_time, 
-                         m=m, target_n=(target_n - n_p)) + int_t
-    } else {
-      # For external, we want setup times for all m sites
-      setup_times <- t(sapply(setup_rates, function(x) cumsum(rexp(m, x))))
-      
-      site_matrix <- cbind(us, setup_times)
-      
-      rec_times <- apply(site_matrix, 1, post_pred_rec_time, 
-                         m=m, target_n=target_n)
-      
-    }
+                         m=m, target_n=(problem$N - n_p)) + problem$p_t
     
   } else {
     # Case where all sites in the main have also opened in the pilot
     # Get the posterior samples for the sites which have already
     # opened (i.e. all sites)
-    r <- ranef(fit, summary = F)
+    r <- brms::ranef(fit, summary = F)
     us <- exp(r$c[,1:m_p,1] + beta_0)
     
-    if(internal){
       # Add pilot sites using a setup time of 0
       setup_times <- matrix(rep(0, m_p*nrow(us)), ncol = m_p)
       
       site_matrix <- cbind(us, setup_times)
       
       rec_times <- apply(site_matrix, 1, post_pred_rec_time, 
-                         m=m, target_n=(target_n - n_p)) + int_t
-    } else {
-      # For external, we want setup times for all m sites
-      setup_times <- t(sapply(setup_rates, function(x) cumsum(rexp(m, x))))
-      
-      site_matrix <- cbind(us, setup_times)
-      
-      rec_times <- apply(site_matrix, 1, post_pred_rec_time, 
-                         m=m, target_n=target_n)
-    }
+                         m=m, target_n=(problem$N - n_p)) + problem$p_t
   }
   
-  return(mean(rec_times))
-  
-  # Bayes_analysis(int_data, 20, bayes_model, 0.5, 300)
+  new_fahb_analysis(c(n_p=n_p, m_p=m_p, r_p=r_p), c(exp_pp_T=mean(rec_times)),
+                    rec_times, so_post_hps = c(setup_r_a1, setup_r_b1),
+                    fit)
 }
 
 post_pred_rec_time <- function(post_samples, m, target_n){
@@ -152,13 +131,9 @@ post_pred_rec_time <- function(post_samples, m, target_n){
   ## Determine how many people need to arrive in that final period
   n_needed <- target_n  -  sum(rec_rates[1:(fin_period-1), 4])
   ## Simulate when the target is hit, which follows a gamma distribution
-  rec_time <- rec_rates[fin_period, 1] + rgamma(1, shape = n_needed, rate = rec_rates[fin_period, 3])
+  rec_time <- rec_rates[fin_period, 1] + stats::rgamma(1, shape = n_needed, rate = rec_rates[fin_period, 3])
   
   return(rec_time)
-  
-  # post_pred_rec_time(c(5,6,3, 0,0.5,1), 3, 200)
-}
-  
 }
 
 compile_bayes_model <- function(n_pilot, t_pilot,
